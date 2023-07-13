0e2d8c7056988fce571be92f1db7723ee28099c7
sonu choure <131000134+choure123@users.noreply.github.com>
2023-07-13 17:48:02 +0530

    ADDED: Static for Example

---
+++ src/Statickeyword.java
@@ -0,0 +1,12 @@
+public class Statickeyword {
+    static void m1()
+    {
+        System.out.println("from m1");
+
+    }
+
+    public static void main(String[] args) {
+        m1();
+    }
+}
+//statickeyword
